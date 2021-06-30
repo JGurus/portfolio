d47312eb3182f80feeccc0ab2dd3d701604ae7a9
JGurus <ljgurumendi@gmail.com>
2021-06-29 21:06:38 -0500

    Updates

--- src/components/Proyects.svelte
+++
@@ -1,9 +0,0 @@
-<section id="proyects" />
-
-<style>
-  #proyects {
-    height: 100vh;
-    background-color: var(--gris);
-    box-shadow: inset 0 -1rem 3rem -30px #000, inset 0 1rem 3rem -30px #000;
-  }
-</style>
